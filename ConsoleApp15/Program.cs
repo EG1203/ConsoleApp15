e655805d5bdeed2ad1a8da2ba6e910bccba6f428
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-21 19:21:19 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString());
+            Console.WriteLine($"it`s{now.Date}, {now.Hour }:{now.Minute}");
+            DateTime dt = new DateTime(2021, 12, 11);
+            DateTime newDT = dt.AddDays(1);
+            Console.WriteLine(newDT);
+            TimeSpan ts = now - dt;
+            //ts=now.Substract(dt);
+            Console.WriteLine(ts.Days);
+        }
+    }
+}
+
+
+
